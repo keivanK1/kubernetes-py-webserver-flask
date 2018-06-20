458d71ac5faf3096e7fe633b59181b0c1656048a
Keivan Kiyanfar <keivan79us@yahoo.com>
2018-06-20 18:45:59 +0200

    Initial Commit

---
+++ api.py
@@ -0,0 +1,32 @@
+# Product service
+# We want to make it restful api, so we will import flask and flask_restful
+# flask is a web framework for python
+
+from flask import Flask
+from flask_restful import Resource, Api
+
+app = Flask(__name__)
+api = Api(app)
+
+class Product(Resource):
+  def get(self):
+    return{
+      'cars':[
+        'BMW',
+        'Benz',
+        'Audi'
+      ]
+    }
+
+api.add_resource(Product, '/')
+
+if __name__=='__main__':
+  app.run(host="0.0.0.0", port=int("800"), debug=True)
+
+# from flask import Flask
+# app = Flask(__name__)
+# @app.route("/")
+# def hello():
+#     return "Hello World!"
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=int("800"), debug=True)
